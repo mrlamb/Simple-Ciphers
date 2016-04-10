88980f766adf2abf4c179a1db8de24983d405e75
MRLamb <lamb.matthew@gmail.com>
2016-04-10 16:51:42 -0500

    Initial Commit - Caesar cipher + example console app

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mlamb.Cipher;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "abcdefghijklmnopqrstuvwxyz", "This is a string", "This is also a string", "Here we change some text", "Text some change we here", "adding punctuation!.?" };
+            foreach (string str in strings)
+            {
+                Console.WriteLine(str);
+                foreach (char letter in str)
+                {
+                    Console.Write(Caesar.Encrypt(letter, 3));
+                }
+                Console.Write("\n");
+            }
+            string string2 = "defghijklmnopqrstuvwxyzabc";
+            foreach (char c in string2)
+            {
+                Console.Write(Caesar.Decrypt(c, 3));
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ mlamb.cipher/Caesar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mlamb.Cipher
+{
+/// <summary>
+/// Implementation of a basic Caesar or Rotational cipher. Not intended to be secure by any means, use for
+/// entertainment purposes only!
+/// </summary>
+    public static class Caesar
+    {
+        /// <summary>
+        /// Returns a char rotated by position.
+        /// </summary>
+        /// <param name="letter">The char to rotate.</param>
+        /// <param name="position">How many positions to rotate by. Should be positive.</param>
+        /// <returns></returns>
+        public static char Encrypt(char letter, int position)
+        {
+            position %= 26;
+            if (char.IsLetter(letter))
+            {
+                if (char.IsUpper(letter))
+                    return (letter + position) > 90 ? (char)((letter + position) - 26) : (char)(letter + position);
+                else
+                    return (letter + position) > 122 ? (char)((letter + position) - 26) : (char)(letter + position);
+            }
+            return letter;
+        }
+        /// <summary>
+        /// Returns a char reverse rotated by position.
+        /// </summary>
+        /// <param name="letter">The char to rotate.</param>
+        /// <param name="position">How many positions to rotate by. Should be positive.</param>
+        /// <returns></returns>
+        public static char Decrypt(char letter, int position)
+        {
+            position %= 26;
+            if (char.IsLetter(letter))
+            {
+                if (char.IsUpper(letter))
+                    return (letter - position) < 65 ? (char)((letter - position) + 26) : (char)(letter - position);
+                else
+                    return (letter - position) < 97 ? (char)((letter - position) + 26) : (char)(letter - position);
+            }
+            return letter;
+        }
+    }
+}
